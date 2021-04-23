0a13a02ec26eb0cc8530f3c36fd429b554f2f2e8
Karol Burylo <kburylo@sii.pl>
2021-04-23 19:00:22 +0000

    Button support

--- src/tasks/intro_tasks.c
+++ src/tasks/intro_tasks.c
@@ -2,6 +2,10 @@
 #include <intro_tasks.h>
 #include <gpio.h>
 #include <utils.h>
+#include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/exti.h>
+#include <libopencm3/stm32/l4/nvic.h>
 
 static void task_producer_run(void* pvParameters);
 static void task_consumer_run(void* pvParameters);
@@ -9,20 +13,38 @@ static void task_async_flasher_run(void* pvParameters);
 
 static intro_tasks itasks;
 
-/*
-    This is stupid not needed comment 
-    
-    This is producer task which sends notification to 
-    consumer task.
+static volatile int counter;
 
-    Why do you think this comment is stupid?
-*/
+void exti15_10_isr()
+{
+    BaseType_t higherTaskWoken;
+    exti_reset_request(EXTI13);
+    if (NULL != itasks.producer_handler) {
+        xTaskNotifyFromISR(itasks.producer_handler,0,eIncrement,&higherTaskWoken);
+    }
+}
+
 static void task_producer_run( void * pvParameters )
 {
     UNUSED(pvParameters);
+    uint32_t notifications = 0;
+
+
+    rcc_periph_clock_enable(RCC_GPIOC);
+    rcc_periph_clock_enable(RCC_SYSCFG);
+
+    gpio_mode_setup(GPIOC,GPIO_MODE_INPUT,GPIO_PUPD_NONE,GPIO13);
+    exti_select_source(EXTI13,GPIOC);
+    exti_set_trigger(EXTI13,EXTI_TRIGGER_FALLING);
+    exti_enable_request(EXTI13);
+
+    nvic_set_priority(NVIC_EXTI15_10_IRQ,128);
+    nvic_enable_irq(NVIC_EXTI15_10_IRQ);
+
     for( ;; )
     {
-        vTaskDelay(PRODUCER_TASK_PERIOD_TICK);
+        xTaskNotifyWait(pdFALSE,0,&notifications,portMAX_DELAY);
+        printf("interrupts %ld\n",notifications);
         gpio_toggle(GPIOC,GPIO9);
         xTaskNotifyGive(itasks.consumer_handler);
     }
@@ -35,6 +57,7 @@ static void task_consumer_run( void * pvParameters )
     {
         ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
         vTaskDelay(CONSUMER_TASK_PERIOD_TICK);
+        printf("I am alive\n");
     }
 }
 
